7cea1d1fc21ad1b837f4e6db8c116199748d66e8
George K. Thiruvathukal <gkt@cs.luc.edu>
2022-04-03 00:23:53 -0500

    more testing and handling of set/unset

--- ssolver.go
+++ ssolver.go
@@ -65,6 +65,18 @@ func (sudoku *Sudoku) checkSubPuzzle(p int, q int) bool {
 	return usedInSubPuzzle.size() == DIGITS
 }
 
+func (sudoku *Sudoku) isFullWithSize() (bool, int) {
+	size := 0
+	for i := 0; i < PDIM; i++ {
+		for j := 0; j < PDIM; j++ {
+			if sudoku.puzzle[i][j] > 0 {
+				size++
+			}
+		}
+	}
+	return size == PDIM*PDIM, size
+}
+
 func (sudoku *Sudoku) show() {
 	for i := range sudoku.puzzle {
 		for j := range sudoku.puzzle[i] {
@@ -79,7 +91,17 @@ func (sudoku *Sudoku) show() {
 	fmt.Println()
 }
 
-func (sudoku *Sudoku) checkSolution() bool {
+func (sudoku *Sudoku) getRepresentation() string {
+	var builder strings.Builder
+	for i := range sudoku.puzzle {
+		for j := range sudoku.puzzle[i] {
+			builder.WriteString(strconv.Itoa(sudoku.puzzle[i][j]))
+		}
+	}
+	return builder.String()
+}
+
+func (sudoku *Sudoku) checkPuzzleValidity() bool {
 	for i := range sudoku.puzzle {
 		if sudoku.rowUsed[i].size() < PDIM {
 			return false
@@ -112,10 +134,24 @@ func (sudoku *Sudoku) setPuzzleValue(i int, j int, value int) {
 	if value > 0 && value < 10 {
 		sudoku.rowUsed[i].add(value)
 		sudoku.columnUsed[j].add(value)
-		sudoku.puzzle[i][j] = value
+	}
+	sudoku.puzzle[i][j] = value
+}
+
+func (sudoku *Sudoku) unsetPuzzleValue(i int, j int) {
+	if i < 0 || i > PDIM {
+		return
+	}
+	if j < 0 || j > PDIM {
+		return
 	}
 
+	value := sudoku.puzzle[i][j]
+	sudoku.rowUsed[i].remove(value)
+	sudoku.columnUsed[j].remove(value)
+	sudoku.puzzle[i][j] = 0
 }
+
 func (sudoku *Sudoku) loadData(text string) {
 	if len(text) < 81 {
 		return
@@ -125,6 +161,7 @@ func (sudoku *Sudoku) loadData(text string) {
 		digit, err := strconv.Atoi(ch)
 		if err != nil {
 			fmt.Printf("Error converting string to int %s\n", ch)
+			break
 		}
 		row := i / 9
 		col := i % 9
@@ -133,16 +170,71 @@ func (sudoku *Sudoku) loadData(text string) {
 	}
 }
 
+func (sudoku *Sudoku) findNextUnfilled(row int, col int) (int, int, bool) {
+	var i int
+	var j int
+	for i = row; i < PDIM; i++ {
+		for j = col; j < PDIM; j++ {
+			if sudoku.puzzle[i][j] == 0 {
+				return i, j, true
+			}
+		}
+	}
+	return -1, -1, false
+}
+
+func (sudoku *Sudoku) isAllowedHere(row int, col int, value int) bool {
+	if sudoku.puzzle[row][col] != 0 {
+		return false
+	}
+	return !(sudoku.rowUsed[row].contains(value) || sudoku.columnUsed[col].contains(value))
+}
+
+func (sudoku *Sudoku) solve() {
+
+}
+
+func isSolution(puzzle string, solution string) (bool, int) {
+	if len(puzzle) != len(solution) {
+		return false, -1
+	}
+	for i := range puzzle {
+		puzzleChar := puzzle[i : i+1]
+		solutionChar := solution[i : i+1]
+		puzzleVal, _ := strconv.Atoi(puzzleChar)
+		solutionVal, _ := strconv.Atoi(solutionChar)
+		if puzzleVal == 0 {
+			continue
+		}
+		if puzzleVal != solutionVal {
+			return false, i
+		}
+	}
+	return true, len(puzzle)
+}
+
+func getDigits(text string) []int {
+	digits := make([]int, len(text))
+	for i := 0; i < len(text); i++ {
+		digit, err := strconv.Atoi(text[i : i+1])
+		if err != nil {
+			break
+		}
+		digits[i] = digit
+	}
+	return digits
+}
+
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Sudoku Solver - working only on tests for now")
 	sudoku := getSudoku()
 	sudoku.loadData("004300209005009001070060043006002087190007400050083000600000105003508690042910300")
 	sudoku.show()
-	unsolved := sudoku.checkSolution()
+	unsolved := sudoku.checkPuzzleValidity()
 	fmt.Println()
 	sudoku.loadData("864371259325849761971265843436192587198657432257483916689734125713528694542916378")
 	sudoku.show()
-	solved := sudoku.checkSolution()
+	solved := sudoku.checkPuzzleValidity()
 	fmt.Printf("Unsolved solution should be false (%t)\n", unsolved)
 	fmt.Printf("Solved solution should be true (%t)\n", solved)
 }
---
+++ sudoku_test.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestSudokuRepresentation(t *testing.T) {
+	sudoku := getSudoku()
+	puzzle := "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+	sudoku.loadData(puzzle)
+	representation := sudoku.getRepresentation()
+	if puzzle != representation {
+		t.Error("String representation not correct")
+	}
+}
+
+func TestSudokuLoaderChecker(t *testing.T) {
+	sudoku := getSudoku()
+	puzzle := "004300209005009001070060043006002087190007400050083000600000105003508690042910300"
+	solution := "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+	sudoku.loadData(puzzle)
+	unsolved := !sudoku.checkPuzzleValidity()
+
+	if !unsolved {
+		t.Error("blank puzzle reported solved - bad")
+	}
+	sudoku.loadData(solution)
+	solved := sudoku.checkPuzzleValidity()
+	if !solved {
+		t.Error("known valid solution to blank puzzle should be solved")
+	}
+
+	expectedSolution, position := isSolution(puzzle, solution)
+	if !expectedSolution {
+		t.Errorf("known puzzle and solution are not a match (position = %d)", position)
+	}
+}
+
+func TestSetUnset(t *testing.T) {
+	knownSolution := "864371259325849761971265843436192587198657432257483916689734125713528694542916378"
+	digits := getDigits(knownSolution)
+	sudoku := getSudoku()
+
+	added := 0
+	for i := 0; i < len(digits); i += 4 {
+		row := i / PDIM
+		col := i % PDIM
+		sudoku.setPuzzleValue(row, col, digits[i])
+		added++
+		_, size := sudoku.isFullWithSize()
+		if size != added {
+			t.Errorf("Size mismatch %d != %d", size, added)
+		}
+	}
+
+}

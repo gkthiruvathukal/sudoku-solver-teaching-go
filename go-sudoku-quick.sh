7543e69675b2c829d150c76757068b45aefe84b1
George K. Thiruvathukal <gkt@cs.luc.edu>
2022-04-07 21:14:39 -0500

    add acceptance test

---
+++ go-sudoku-quick.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+i=0
+status=0
+for filename in data/*-sample-*.txt; do
+   while IFS=',' read -r col1 col2; do
+      i=$(($i + 1))
+      echo "Solving Puzzle $i"
+      echo "$col1"
+      echo "$col2"
+      ./sudoku_solver --puzzle "$col1" 
+      status=$(($status + $?))
+      if [ $? == 0 ]; then
+         echo "Success"
+      else
+         echo "Failure"
+      fi
+   done < $filename
+done
+
+echo "Exiting with $status"
+echo $status

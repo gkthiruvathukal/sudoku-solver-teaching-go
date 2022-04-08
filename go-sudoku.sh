81639014d8c967682adf89dbdefde55aa5073468
George K. Thiruvathukal <gkt@cs.luc.edu>
2022-04-07 21:37:19 -0500

    update testing scripts

--- go-sudoku.sh
+++ go-sudoku.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
 i=0
-for filename in data/*-[a-z].txt; do
+success=0
+for filename in data/*-[a-b].txt; do
    while IFS=',' read -r col1 col2; do
       i=$(($i + 1))
       echo "Solving Puzzle $i"
       echo "$col1"
       echo "$col2"
-      ./sudoku_solver --puzzle "$col1"
+      ./sudoku_solver --puzzle "$col1" 
+      success=$(($success + $?))
+      if [ $? == 0 ]; then
+         echo "Success"
+      else
+         echo "Failure"
+      fi
    done < $filename
 done
+
+exit $success
